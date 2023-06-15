c30a5bcdeca9ee093b6a885b4cffa6e8bfe0177c
Валентин Горовенко <Punker8381@yandex.ru>
2023-06-16 01:50:38 +0700

    1.0

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['158.160.54.148', '127.0.0.1', 'localhost']
 
 
 # Application definition
