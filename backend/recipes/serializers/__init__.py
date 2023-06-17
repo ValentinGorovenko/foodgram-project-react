4b12c358789f12b2d67a6098ffea853afbfb5520
Валентин Горовенко <Punker8381@yandex.ru>
2023-06-18 01:20:49 +0700

    v7.3

--- backend/recipes/serializers/__init__.py
+++ backend/recipes/serializers/__init__.py
@@ -1,8 +1,12 @@
 from .favorite_serializer import FavoriteSerializer  # noqa: F401
-from .ingredients_recipe_serializer import IngredientRecipeSerializer  # noqa: F401
+from .ingredients_recipe_serializer import (  # noqa: F401
+    IngredientRecipeSerializer,  # noqa: F401
+)
 from .ingredients_serializers import IngredientSerializer  # noqa: F401
 from .recipe_create_serializer import RecipeCreateSerializer  # noqa: F401
-from .recipe_mini_fields_serializer import RecipeMiniFieldsSerializer  # noqa: F401
+from .recipe_mini_fields_serializer import (  # noqa: F401
+    RecipeMiniFieldsSerializer,  # noqa: F401
+)
 from .recipe_serializer import RecipeSerializer  # noqa: F401
 from .shopping_list_serializer import ShoppingListSerializer  # noqa: F401
 from .tags_serializers import TagSerializer  # noqa: F401
